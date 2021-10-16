816abe185fd3017962f2ad92bbaf03177656e37a
IbrahimNabid <79735610+IbrahimNabid@users.noreply.github.com>
2021-10-16 18:34:58 -0400

    Initial Commit

---
+++ iOS tip app prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS tip app prework
+//
+//  Created by Ibrahim Nabid on 10/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
