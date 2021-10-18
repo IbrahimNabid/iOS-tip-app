667978be51770bc9086de655f2ea19f5e77a786c
IbrahimNabid <79735610+IbrahimNabid@users.noreply.github.com>
2021-10-17 20:03:06 -0400

    Updated ViewController.swift
    Added a stepper to split tips

--- iOS tip app prework/ViewController.swift
+++ iOS tip app prework/ViewController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var splitValue: UIStepper!
+    @IBOutlet weak var splitNumber: UILabel!
+    @IBOutlet weak var splitTotal: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let sValue = Int(splitValue.value)
+        let sTotal = total/Double(sValue)
+                
+        splitTotal.text = String(format: "$%.2f", sTotal)
+        splitNumber.text = String(format: "%2d%", sValue)
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
 
 
 }
